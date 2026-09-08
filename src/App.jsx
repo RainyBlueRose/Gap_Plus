491c1ef602a86aae51813f3691ca65b26e71469e
Khaotang <tangtangja1234@gmail.com>
2026-09-08 13:49:54 +0700

    setup Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { router } from "./components/router/Router";
+import { RouterProvider } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <RouterProvider router={router} />;
+    </div>
+  );
+};
 
-export default App
+export default App;
