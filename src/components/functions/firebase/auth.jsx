7078b755ff69e3aafe7c16af3c9ff532b88c8c25
Khaotang <tangtangja1234@gmail.com>
2026-09-09 09:42:13 +0700

    feat(auth):add Signout function

--- src/components/functions/firebase/auth.jsx
+++ src/components/functions/firebase/auth.jsx
@@ -1,7 +1,12 @@
 import { auth, googleAuthProvider } from "../../config/firebase";
-import { signInWithPopup } from "firebase/auth";
+import { signInWithPopup, signOut } from "firebase/auth";
 
 export const handleSigninByGoogle = async () => {
   const result = await signInWithPopup(auth, googleAuthProvider);
   console.log("result", result);
+  console.log("token", await result.user.getIdTokenResult());
+};
+
+export const handleSignout = async () => {
+  signOut()
 };
