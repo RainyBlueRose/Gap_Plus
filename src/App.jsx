cffad7f35dec5d9ea3b0e1da7a3d26e2705e78d0
Khaotang <tangtangja1234@gmail.com>
2026-09-09 16:21:38 +0700

    wip(App):fix navigate error and sorting function files

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import React, { useEffect } from "react";
 import { router } from "./components/router/Router";
-import { RouterProvider, Navigate } from "react-router-dom";
+import { RouterProvider, useNavigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./components/config/firebase";
 
 const App = () => {
+  const navigate = useNavigate();
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log("เปิดonAuthStateChanged");
       if (!user) {
-        return <Navigate to="/Home" />;
+        console.log("ไม่พบ User");
+        navigate("/");
+        return;
       }
+      console.log("พบ User");
+      navigate("/Home");
+      return;
     });
     return () => {
+      console.log("ปิดonAuthStateChanged");
       unsubscribe();
     };
   }, []);
