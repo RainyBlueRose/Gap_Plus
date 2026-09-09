ae39745a80c826c8c9c542b78623382a7fca0ebe
Khaotang <tangtangja1234@gmail.com>
2026-09-09 14:56:55 +0700

    feat(App):Checking JWT when re render -using useEffect and onAuthStateChanged

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { router } from "./components/router/Router";
 import { RouterProvider } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./components/config/firebase";
 
 const App = () => {
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log("user", user);
+    });
+  }, []);
   return (
     <div>
       <RouterProvider router={router} />
