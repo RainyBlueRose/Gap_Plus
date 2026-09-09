3e18aa7bd9f2c494ca3525ddd0154e14585c0a3f
Khaotang <tangtangja1234@gmail.com>
2026-09-09 15:19:45 +0700

    feat(Router):create /Home path to Home page

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
+import Home from "../pages/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Login />,
   },
+  { path: "/Home", element: <Home /> },
 ]);
