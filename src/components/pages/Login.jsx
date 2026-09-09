72daf6e3927f4ff8cde606d4ded36c0afa16cfae
Khaotang <tangtangja1234@gmail.com>
2026-09-09 12:02:24 +0700

    feat(Login):add Login by Google button include onClick function -import useSocialSignin from hooks -add onClick={signIn(google) to Login By Google button

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { handleSignout } from "../functions/firebase/auth";
+import { useSocialSignIn } from "../functions/hooks/useSocialSignIn";
 
 const Login = () => {
+  const { signIn } = useSocialSignIn();
+
   return (
     <div>
       <div>
-        <button>Login</button>
+        <button onClick={signIn("google")}>Login by Google</button>
         <button onClick={handleSignout}>Logout</button>
       </div>
     </div>
