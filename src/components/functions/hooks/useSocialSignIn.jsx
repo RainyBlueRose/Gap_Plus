ec0c8c0996544c1799fb6d7640adfcaeeed780e9
Khaotang <tangtangja1234@gmail.com>
2026-09-09 11:53:54 +0700

    feat(useSocialSignin):signIn function call provider depend on argument(provider name) -add signIn functions to useSocialSignIn function -signIn functions job is calling signin popup for each provider and save user info to redux

--- src/components/functions/hooks/useSocialSignIn.jsx
+++ src/components/functions/hooks/useSocialSignIn.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { signInWithProvider } from "../firebase/authService";
 
 const useSocialSignIn = () => {
   const dispatch = useDispatch();
+
+  const signIn = async (providerName) => {
+    try {
+      const result = await signInWithProvider(providerName);
+    } catch (err) {
+      console.error(`${providerName} signin failed`, err);
+    }
+  };
 };
 
 export default useSocialSignIn;
