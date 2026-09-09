ceadf83e05b5b3f7b46b4153c26708f2e0b50452
Khaotang <tangtangja1234@gmail.com>
2026-09-09 14:27:42 +0700

    feat(Login):make Logout button usable -import useAuth -declare const signOut from useAuth -add signOut function to onClick

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { handleSignout } from "../functions/firebase/authService";
 import { useSocialSignIn } from "../functions/hooks/useSocialSignIn";
+import { useAuth } from "../functions/hooks/useAuth";
 
 const Login = () => {
   const { signIn } = useSocialSignIn();
+  const { signOut } = useAuth();
 
   return (
     <div>
@@ -15,7 +17,13 @@ const Login = () => {
         >
           Login by Google
         </button>
-        <button onClick={handleSignout}>Logout</button>
+        <button
+          onClick={() => {
+            signOut();
+          }}
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
