39ee5262848c83934d141b8d9b8fe6d123cbf121
Khaotang <tangtangja1234@gmail.com>
2026-09-10 10:05:15 +0700

    feat(Router): add ProtectedRoute to Home element

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
+
+import { ProtectedRoute } from "../functions/hooks/ProtectedRoute";
+
 import Login from "../pages/Login";
 import Home from "../pages/Home";
 
@@ -8,5 +11,8 @@ export const router = createBrowserRouter([
     path: "/",
     element: <Login />,
   },
-  { path: "/Home", element: <Home /> },
+  {
+    element: <ProtectedRoute />,
+    children: [{ path: "/Home", element: <Home /> }],
+  },
 ]);
