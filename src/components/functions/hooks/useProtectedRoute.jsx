fa36ef5d6bba0fcda68420bd5ec8bbd5abe2cbe5
Khaotang <tangtangja1234@gmail.com>
2026-09-10 09:42:50 +0700

    feat(useProtectedRoute):Checking if having user from userSlice

--- src/components/functions/hooks/useProtectedRoute.jsx
+++ src/components/functions/hooks/useProtectedRoute.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-const useProtectedRoute = () => {};
+import { useSelector } from "react-redux";
+import { selectUsers } from "../../../store/userSlice";
+
+const useProtectedRoute = () => {
+  const { user } = useSelector(selectUsers);
+
+  const protectedRoute = () => {
+    if (user) {
+      console.log("มี User");
+      return;
+    }
+    console.log("ไม่มี User");
+  };
+  return { protectedRoute };
+};
 
 export default useProtectedRoute;
