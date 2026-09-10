8f63b4ae5bc2ceddf797f5ce9bc10e3a696677e5
Khaotang <tangtangja1234@gmail.com>
2026-09-10 10:26:42 +0700

    feat(useAuth): navigate to Login after signOut

--- src/components/functions/hooks/useAuth.jsx
+++ src/components/functions/hooks/useAuth.jsx
@@ -4,13 +4,16 @@ import { useDispatch } from "react-redux";
 
 import { handleSignout } from "../firebase/authService";
 import { logout } from "../../../store/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export const useAuth = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const signOut = async () => {
     await handleSignout();
     dispatch(logout());
+    navigate("/");
   };
 
   return { signOut };
