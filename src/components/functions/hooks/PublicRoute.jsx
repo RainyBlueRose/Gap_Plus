206fc679e6a1be94b248c12417a9a70c81c4c3da
Khaotang <tangtangja1234@gmail.com>
2026-09-10 14:56:10 +0700

    feat(PublicRoute): when user already login navigate user to Home page

--- src/components/functions/hooks/PublicRoute.jsx
+++ src/components/functions/hooks/PublicRoute.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { selectUser } from "../../../store/userSlice";
+import { ROUTES } from "../../router/Router";
 
-const PublicRoute = () => {};
+const PublicRoute = () => {
+  const { isAuthentication, authInitialize } = useSelector(selectUser);
+
+  if (authInitialize) {
+    return <div>loading ...</div>;
+  }
+
+  if (isAuthentication) {
+    return <Navigate to={ROUTES.HOME} replace />;
+  }
+};
 
 export default PublicRoute;
