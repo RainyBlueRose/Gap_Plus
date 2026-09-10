64b6cabf9d4387e855f6278814588e12cd4d179a
Khaotang <tangtangja1234@gmail.com>
2026-09-10 09:16:39 +0700

    fix(App):remove navigator and useEffect cause Navigator's Error

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,6 @@ import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./components/config/firebase";
 
 const App = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      console.log("เปิดonAuthStateChanged");
-      if (!user) {
-        console.log("ไม่พบ User");
-        navigate("/");
-        return;
-      }
-      console.log("พบ User");
-      navigate("/Home");
-      return;
-    });
-    return () => {
-      console.log("ปิดonAuthStateChanged");
-      unsubscribe();
-    };
-  }, []);
   return (
     <div>
       <RouterProvider router={router} />
