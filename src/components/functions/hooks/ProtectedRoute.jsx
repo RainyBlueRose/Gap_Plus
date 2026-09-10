daaaaae7edde9b303d7830d7fa605a378d55f7f3
Khaotang <tangtangja1234@gmail.com>
2026-09-10 10:03:36 +0700

    fix(ProtectedRoute):export ProtectedRoute

--- src/components/functions/hooks/ProtectedRoute.jsx
+++ src/components/functions/hooks/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectUsers } from "../../../store/userSlice";
 
-const ProtectedRoute = () => {
+export const ProtectedRoute = () => {
   const { user } = useSelector(selectUsers);
 
   if (!user) {
