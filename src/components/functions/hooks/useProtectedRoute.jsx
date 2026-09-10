8666deb0371c5398cdfbe626687141763c6520ce
Khaotang <tangtangja1234@gmail.com>
2026-09-10 09:56:28 +0700

    fix(useProtectedRoute):remove nested function(protectedRoute) -in this case we need function to run everytime when re-render not like signIn we need it to run only when user click also we need argument from user so we can't run it everytime it's re-render

--- src/components/functions/hooks/useProtectedRoute.jsx
+++ src/components/functions/hooks/useProtectedRoute.jsx
@@ -6,14 +6,11 @@ import { selectUsers } from "../../../store/userSlice";
 const useProtectedRoute = () => {
   const { user } = useSelector(selectUsers);
 
-  const protectedRoute = () => {
-    if (user) {
-      console.log("มี User");
-      return;
-    }
-    console.log("ไม่มี User");
-  };
-  return { protectedRoute };
+  if (user) {
+    console.log("มี User");
+    return;
+  }
+  console.log("ไม่มี User");
 };
 
 export default useProtectedRoute;
