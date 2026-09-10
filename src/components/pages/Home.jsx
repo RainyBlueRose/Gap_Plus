8fd3131992e4d186645bce9dfada05248f1342f3
Khaotang <tangtangja1234@gmail.com>
2026-09-10 15:42:18 +0700

    feat(Home): add Signout Button

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+
+import { useAuth } from "../functions/hooks/useAuth";
 
 const Home = () => {
+  const { signOut } = useAuth();
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <p>Home</p>
+      <button
+        onClick={() => {
+          signOut();
+        }}
+      >
+        {" "}
+        Sing Out{" "}
+      </button>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
