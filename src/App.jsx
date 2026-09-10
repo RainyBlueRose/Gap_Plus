64279a8e94d44ed561eb57fa9466e79c52e25eed
Khaotang <tangtangja1234@gmail.com>
2026-09-10 13:43:25 +0700

    feat(App): calling function useAuthListener

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { router } from "./components/router/Router";
 import { RouterProvider, useNavigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./components/config/firebase";
+import { useAuthListener } from "./components/functions/hooks/useAuthListener";
 
 const App = () => {
+  useAuthListener();
   return (
     <div>
       <RouterProvider router={router} />
